cbfc70b534b94ade9ca7a3694bcb2df0909256ec
ns25304019 <ns25304019@ga.ttc.ac.jp>
2025-11-21 14:55:23 +0900

    hello

---
+++ pr_four.java
@@ -0,0 +1,48 @@
+abstract class Product {
+    private String _name;
+    private int _price;
+
+    public Product(String name, int price) {
+        _name = name;
+        _price = price;
+    }
+    public String getName() {
+        return _name;
+    }
+    public int getPrice() {
+        return _price;
+    }
+    public abstract void work();
+}
+class PrinterProduct extends Product {
+    public PrinterProduct(String name, int price) {
+        super(name, price);
+    }
+    public void work() {
+        System.out.println("印刷します。");
+    }
+
+     
+}
+class CameraProduct extends Product {
+    public CameraProduct(String name, int price) {
+        super( name, price);
+    }
+    public void work() {
+        System.out.println("撮影します。");
+    }
+}
+class Executer{
+    public void doProduct(Product p) {
+        p.work();
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        PrinterProduct p1 = new PrinterProduct("AAA", 15000);
+        CameraProduct p2 = new CameraProduct("BBB", 30000);
+        Executer e = new Executer();
+        e.doProduct(p1);
+        e.doProduct(p2);
+    }
+}
